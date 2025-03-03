237ce7eab02b653a892d4d988b68313ee413ed80
iain-code <iainvirgo1010@live.co.uk>
2025-03-03 09:13:47 +0200

    changed name

--- main.go
+++ main.go
@@ -8,12 +8,15 @@ import (
 func main() {
 
 	const port = "8080"
-	mux := http.NewServeMux()
+	mux := http.NewServeMux() // checks each request for keywords to know how to handle each request
 
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
+	fileServer := http.FileServer(http.Dir("."))
+	mux.Handle("/", fileServer)
 	log.Printf("Serving on port: %s\n", port)
-	log.Fatal(srv.ListenAndServe())
+	log.Fatal(srv.ListenAndServe()) // starts the server and will "listen" for http requests on given ports and return errors
+
 }
